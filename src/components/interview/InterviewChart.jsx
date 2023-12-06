f60de0c7fea8e3902e13d908030f13f3af6b47cf
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-06 19:23:13 +0900

    [FIX] Interview chart에 NaN 뜨던 오류 수정

--- src/components/interview/InterviewChart.jsx
+++ src/components/interview/InterviewChart.jsx
@@ -9,7 +9,7 @@ function InterviewChart({ title, countData, total }) {
           return (
             <div key={label + value}>
               <p>{label}</p>
-              <p>{(count / total).toFixed(2) * 100}%</p>
+              <p>{(count / total || 0).toFixed(2) * 100}%</p>
             </div>
           );
         })}
