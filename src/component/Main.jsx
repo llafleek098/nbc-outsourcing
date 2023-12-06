15d2ba668c5e1b19e3c0b7a3501957a8e7ff6fd0
Liabinn <liabin124@gmail.com>
2023-12-07 04:17:09 +0900

    반응형 웹 구현 2차 완료

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -2,16 +2,61 @@ import React from 'react'
 import bg1 from '../assets/img/main_bg1.jpg'
 import bg2 from '../assets/img/main_bg2.jpg'
 import bg3 from '../assets/img/main_bg3.jpg'
+import logo from '../assets/img/Logo.png'
 import styled from 'styled-components'
+// import { useNavigate } from 'react-router-dom'
 
 function Main() {
+
+  // const navigate = useNavigate();
+
+  // const handleNavigate = (e) => {
+  //   console.log(e.target.name)
+  //   const {name} = e.target
+  //   switch (name) {
+  //     case 'Menu':
+  //       console.log('메뉴로 이동합니다.')
+  //       // navigate('/')
+  //       break;
+  //     case 'Favorite':
+  //       console.log('선호도 조사폼으로 이동합니다.')
+  //       // navigate('/')
+  //       break;
+  //     case 'Store':
+  //       console.log('매장안내로 이동합니다.')
+  //       // navigate('/')
+  //       break;
+  //     default:
+  //       return;
+  //   }
+  // }
+
+  const handleOnClickMenu = () => {
+    console.log('메뉴로 이동합니다.')
+    // navigate('/')
+  }
+  const handleOnClickFavorite = () => {
+    console.log('선호도 조사폼으로 이동합니다.')
+    // navigate('/')
+  }
+  const handleOnClickStore = () => {
+    console.log('매장안내로 이동합니다.')
+    // navigate('/')
+  }
+
   return (
     <div>
-      <StYouTubeWrapper><div style={{border: '2px solid red', height: '100%', width: '100%', maxWidth: '1280px', maxHeight: '720px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '2rem'}}></div></StYouTubeWrapper>
+      <StYouTubeWrapper>
+        <StYouTubeVideo>
+        </StYouTubeVideo>
+      </StYouTubeWrapper>
       <StEntireContainer>
         <StMenuWrapper>
           <StBgImg src={bg1} />
-          <p>PAIK'S MENU</p> 
+          <p>PAIK'S MENU</p>
+          <StMenuButtonBox name={'Menu'} onClick={handleOnClickMenu}>
+            <PlusButton/>
+          </StMenuButtonBox>
         </StMenuWrapper>
         <StFavoriteStoreContainer>
           <StFavoriteWrapper>
@@ -19,10 +64,16 @@ function Main() {
             <p>WHAT'S YOUR<br />
             FAVORITE<br />
             PAIK'S</p>
+            <StFavoriteButtonBox name={'Favorite'} onClick={handleOnClickFavorite}>
+            <PlusButton/>
+            </StFavoriteButtonBox>
           </StFavoriteWrapper>
           <StStoreWrapper>
             <StBgImg src={bg3} />
             <p>PAIK'S STORE</p>
+            <StStoreButtonBox name={'Store'} onClick={handleOnClickStore}>
+            <PlusButton/>
+            </StStoreButtonBox>
           </StStoreWrapper>
         </StFavoriteStoreContainer>
       </StEntireContainer>
@@ -37,13 +88,32 @@ const StYouTubeWrapper = styled.div`
   position: relative;
 `;
 
+const StYouTubeVideo = styled.div`
+  border: 1px solid red;
+  height: 100%;
+  width: 100%;
+  max-width: 1280px;
+  max-height: 720px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2rem;
+`;
+
 const StEntireContainer = styled.div`
   display: flex;
+  font-size: 5rem;
   @media screen and (max-width: 76.8rem) {
     display: flex;
     flex-direction: column;
   }
-`
+  @media screen and (max-width: 37.5rem) {
+    display: flex;
+    flex-direction: column;
+    font-size: 4rem;
+  }
+`;
 
 const StMenuWrapper = styled.div`
   height: 50rem;
@@ -51,11 +121,13 @@ const StMenuWrapper = styled.div`
   position: relative;
   & p {
     color: #6b4d30;
-    font-size: 5rem;
     font-weight: bold;
     position: absolute;
     top: 70%;
-    left: 8%;
+    left: 7%;
+  }
+  &:hover {
+    cursor: pointer;
   }
   @media screen and (max-width: 76.8rem) {
     width: 100%;
@@ -72,7 +144,83 @@ const StBgImg = styled.img`
   height: 100%;
   object-fit: cover;
   margin: auto;
+`;
+
+const StMenuButtonBox = styled.div`
+  width: 5rem;
+  height: 5rem;
+  position: absolute;
+  perspective: 110rem;
+  top: 10%;
+  right: 5%;
+  &:hover {
+    & div {
+      transform: rotateY(180deg);
+    }
+  }
+`;
+
+const StButtonAnimation = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transition: 0.4s;
+  transform-style: preserve-3d;
 `
+
+const StMoveButton = styled.button`
+  background-color: white;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  backface-visibility: hidden;
+  border-radius: 50%;
+  font-size: 4rem;
+  text-align: center;
+  line-height: 1.2;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const StSwitchLogoImg = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const StFavoriteButtonBox = styled.div`
+  width: 5rem;
+  height: 5rem;
+  position: absolute;
+  perspective: 110rem;
+  bottom: 10%;
+  right: 5%;
+  &:hover {
+    & div {
+      transform: rotateY(180deg);
+    }
+  }
+`;
+
+const StStoreButtonBox = styled.div`
+  width: 5rem;
+  height: 5rem;
+  position: absolute;
+  perspective: 110rem;
+  top: 10%;
+  left: 5%;
+  &:hover {
+    & div {
+      transform: rotateY(180deg);
+    }
+  }
+`;
 
 const StFavoriteStoreContainer = styled.div`
   height: 50rem;
@@ -82,21 +230,22 @@ const StFavoriteStoreContainer = styled.div`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
 const StFavoriteWrapper = styled.div`
-  height: 25rem;
+  height: 50%;
   position: relative;
+  &:hover {
+    cursor: pointer;
+  }
   & p {
     color: #071F60;
-    font-size: 5rem;
     font-weight: bold;
     position: absolute;
     top: 13%;
     left: 7%;
     @media screen and (max-width: 76.8rem) {
     color: #071F60;
-    font-size: 5rem;
     font-weight: bold;
     position: absolute;
     top: 17%;
@@ -111,18 +260,19 @@ const StFavoriteWrapper = styled.div`
 `;
 
 const StStoreWrapper = styled.div`
-  height: 25rem;
+  height: 50%;
   position: relative;
+  &:hover {
+    cursor: pointer;
+  }
   & p {
     color: white;
-    font-size: 5rem;
     font-weight: bold;
     position: absolute;
     bottom: 15%;
     right: 5%;
     @media screen and (max-width: 76.8rem) {
     color: white;
-    font-size: 5rem;
     font-weight: bold;
     position: absolute;
     bottom: 15%;
@@ -134,6 +284,15 @@ const StStoreWrapper = styled.div`
     display: flex;
     flex-direction: column;
   }
-`
+`;
+
+const PlusButton = () => {
+  return (
+    <StButtonAnimation>
+      <StMoveButton>+</StMoveButton>
+      <StSwitchLogoImg src={logo} />
+    </StButtonAnimation>
+  )
+}
 
 export default Main;
