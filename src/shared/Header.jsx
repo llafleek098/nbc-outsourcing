a1b00e6637b6feaa11289b3d681fcb40da615740
Jangkaeul <09autumn08@naver.com>
2023-12-07 12:16:15 +0900

    header 커서이벤트추가

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -9,13 +9,24 @@ const HeaderStyle = styled.div`
   height: 100px;
   font-size: 25px;
   font-weight: 600;
+  display: flex;
   justify-content: space-evenly;
+  align-items: center;
+  padding-left: 300px;
+  padding-right: 300px;
 `;
 
 const HeaderButton = styled.button`
   background-color: transparent;
-  font-size: 25px;
+  font-size: 23px;
   font-weight: 600;
+  color: #071f60;
+  cursor: pointer;
+`;
+
+const LogoButton = styled.button`
+  cursor: pointer;
+  background-color: transparent;
 `;
 
 const PaiksCoffeeLogo = styled.img`
@@ -26,13 +37,20 @@ const PaiksCoffeeLogo = styled.img`
 const HeaderContainer = () => {
   return (
     <HeaderStyle>
-      <Link to="/products/:productId">
+      <Link to="/">
+        <LogoButton>
+          <PaiksCoffeeLogo src={paiksLogo} />
+        </LogoButton>
+      </Link>
+      <Link to="/products">
         <HeaderButton>메뉴</HeaderButton>
       </Link>
-      <HeaderButton>매장 안내</HeaderButton>
-      <HeaderButton>고객의 소리</HeaderButton>
-
-      <PaiksCoffeeLogo src={paiksLogo} />
+      <Link to="/location">
+        <HeaderButton>매장 안내</HeaderButton>
+      </Link>
+      <Link to="/interview">
+        <HeaderButton>고객의 소리</HeaderButton>
+      </Link>
     </HeaderStyle>
   );
 };
