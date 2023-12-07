ae33eea8c6d5de2d253f2fa1a0a19ca2fb9e670b
Jangkaeul <09autumn08@naver.com>
2023-12-07 15:21:06 +0900

    태그명수정

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -3,20 +3,41 @@ import React from 'react';
 import styled from 'styled-components';
 import paiksLogo from '../assets/img/header_logo.png';
 
-const HeaderStyle = styled.div`
+const HeaderContainer = () => {
+  return (
+    <StHeaderStyle>
+      <Link to="/">
+        <StLogoButton>
+          <StPaiksCoffeeLogo src={paiksLogo} />
+        </StLogoButton>
+      </Link>
+      <Link to="/products">
+        <StHeaderButton>메뉴</StHeaderButton>
+      </Link>
+      <Link to="/location">
+        <StHeaderButton>매장 안내</StHeaderButton>
+      </Link>
+      <Link to="/interview">
+        <StHeaderButton>고객의 소리</StHeaderButton>
+      </Link>
+    </StHeaderStyle>
+  );
+};
+const StHeaderStyle = styled.div`
   background-color: #e6e7e8;
   width: 100vw;
   height: 100px;
   font-size: 25px;
   font-weight: 600;
   display: flex;
+  /* flex-direction: column; */
   justify-content: space-evenly;
   align-items: center;
   padding-left: 300px;
   padding-right: 300px;
 `;
 
-const HeaderButton = styled.button`
+const StHeaderButton = styled.button`
   background-color: transparent;
   font-size: 23px;
   font-weight: 600;
@@ -24,34 +45,14 @@ const HeaderButton = styled.button`
   cursor: pointer;
 `;
 
-const LogoButton = styled.button`
+const StLogoButton = styled.button`
   cursor: pointer;
   background-color: transparent;
 `;
 
-const PaiksCoffeeLogo = styled.img`
+const StPaiksCoffeeLogo = styled.img`
   margin-left: 20px;
   width: auto;
 `;
 
-const HeaderContainer = () => {
-  return (
-    <HeaderStyle>
-      <Link to="/">
-        <LogoButton>
-          <PaiksCoffeeLogo src={paiksLogo} />
-        </LogoButton>
-      </Link>
-      <Link to="/products">
-        <HeaderButton>메뉴</HeaderButton>
-      </Link>
-      <Link to="/location">
-        <HeaderButton>매장 안내</HeaderButton>
-      </Link>
-      <Link to="/interview">
-        <HeaderButton>고객의 소리</HeaderButton>
-      </Link>
-    </HeaderStyle>
-  );
-};
 export default HeaderContainer;
