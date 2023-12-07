184a8c67a51c77be34c45109a031281d6ed3f068
takoyakki0609 <kjy6478@naver.com>
2023-12-07 14:49:41 +0900

    api 무한호출 수정

--- src/pages/LocationPage.jsx
+++ src/pages/LocationPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Map, MapMarker, MarkerClusterer } from 'react-kakao-maps-sdk';
 import styled from 'styled-components';
 
@@ -6,17 +6,20 @@ function LocationPage() {
   const [info, setInfo] = useState();
   const [markers, setMarkers] = useState([]);
   const [map, setMap] = useState();
-  const [currentPosition, setCurrentPosition] = useState(undefined);
+  const [currentPosition, setCurrentPosition] = useState({
+    lat: 37.50231497199725,
+    lng: 127.04484141806945
+  });
+  const inputRef = useRef();
 
   const searchPaik = () => {
     if (!map) return;
     const ps = new window.kakao.maps.services.Places(map);
-
+    console.log(1);
     ps.keywordSearch(
       '빽다방',
       (data, status, _pagination) => {
-        console.log(_pagination, '_pagination');
-
+        //ref에 넣기 ......
         if (status === window.kakao.maps.services.Status.OK) {
           // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
           // LatLngBounds 객체에 좌표를 추가합니다
@@ -42,7 +45,7 @@ function LocationPage() {
     );
   };
 
-  useEffect(() => {
+  /*   useEffect(() => {
     if (navigator.geolocation) {
       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
       navigator.geolocation.getCurrentPosition(
@@ -67,55 +70,60 @@ function LocationPage() {
         lng: 127.04484141806945
       });
     }
-  }, []);
+  }, []); */
+  useEffect(() => {
+    console.log('map', map);
+    searchPaik();
+  }, [map]);
 
+  const onSearchChanged = () => {};
   return (
     <Container>
-      {currentPosition && (
-        <Map // 로드뷰를 표시할 Container
-          center={currentPosition}
-          style={{
-            width: '100%',
-            height: '350px'
-          }}
-          level={5}
-          onCreate={(map) => {
-            setMap(map);
-            searchPaik();
-          }}
-          onCenterChanged={() => searchPaik()}
+      <Input ref={inputRef} onChange={onSearchChanged} />
+      <Map // 로드뷰를 표시할 Container
+        center={currentPosition}
+        style={{
+          width: '100%',
+          height: '350px'
+        }}
+        level={5}
+        onCreate={(map) => {
+          setMap(map);
+        }}
+        // onCenterChanged={() => searchPaik()}
+        onZoomChanged={() => searchPaik()}
+        onDragEnd={() => searchPaik()}
+      >
+        <MarkerClusterer
+          averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+          minLevel={10} // 클러스터 할 최소 지도 레벨
         >
-          <MarkerClusterer
-            averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-            // minLevel={10} // 클러스터 할 최소 지도 레벨
-          >
-            {markers.map((marker) => (
-              <MapMarker
-                image={{
-                  src: 'https://paikdabang.com/wp-content/themes/paikdabang/assets/images/about1.png', // 마커이미지의 주소입니다
-                  size: {
-                    width: 42,
-                    height: 42
-                  }, // 마커이미지의 크기입니다
-                  options: {
-                    offset: {
-                      x: 27,
-                      y: 69
-                    } // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-                  }
-                }}
-                key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
-                position={marker.position}
-                onClick={() => setInfo(marker)}
-              >
-                {info && info.content === marker.content && (
-                  <div style={{ color: '#000' }}>{marker.content}</div>
-                )}
-              </MapMarker>
-            ))}
-          </MarkerClusterer>
-        </Map>
-      )}
+          {markers.map((marker) => (
+            <MapMarker
+              image={{
+                src: 'https://paikdabang.com/wp-content/themes/paikdabang/assets/images/about1.png', // 마커이미지의 주소입니다
+                size: {
+                  width: 42,
+                  height: 42
+                }, // 마커이미지의 크기입니다
+                options: {
+                  offset: {
+                    x: 27,
+                    y: 69
+                  } // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                }
+              }}
+              key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+              position={marker.position}
+              onClick={() => setInfo(marker)}
+            >
+              {info && info.content === marker.content && (
+                <div style={{ color: '#000' }}>{marker.content}</div>
+              )}
+            </MapMarker>
+          ))}
+        </MarkerClusterer>
+      </Map>
     </Container>
   );
 }
@@ -123,6 +131,7 @@ function LocationPage() {
 export default LocationPage;
 
 const Container = styled.div`
+  position: relative;
   max-width: 1200px;
   min-width: 760px;
   display: flex;
@@ -130,4 +139,14 @@ const Container = styled.div`
   align-items: center;
 
   margin: 0 auto;
+`;
+const Input = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 999;
+
+  border: none;
+  &:focus {
+  }
 `;
