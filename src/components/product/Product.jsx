c0202f57699084a1c46f4f02428484199099461a
Liabinn <liabin124@gmail.com>
2023-12-09 00:46:50 +0900

    상세페이지 헤더css 통일, product 버튼 정렬 수정

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -26,6 +26,7 @@ function Product() {
           alt="product mobile background"
         />
         <h2>PAIK'S MENU</h2>
+        <p>지금 바로 가까운 매장에서 빽다방 메뉴를 만나 보실 수 있습니다.</p>
       </StProductHeaderContainer>
       <main>
         <StSelectCategoryContainer>
@@ -35,7 +36,6 @@ function Product() {
                 to={`/products?category=${category}`}
                 $selectCategory={selectedCategory}
                 key={category}
-                // onClick={handleSelectCategory}
                 value={category}
               >
                 {category}
@@ -70,10 +70,8 @@ function Product() {
 
 // 전체를 감싸는 컨테이너
 const StProductContainer = styled.div`
-  margin: 0 auto;
-  font-weight: bold;
-  div,
-  button {
+  margin: 0 auto 3rem auto;
+  h2 {
     font-weight: bold;
   }
 `;
@@ -83,12 +81,19 @@ const StProductHeaderContainer = styled.header`
   position: relative;
   display: flex;
   justify-content: center;
-  align-items: center;
   & h2 {
     color: #6b4d30;
     letter-spacing: -5px;
     font-size: 5rem;
     position: absolute;
+    top: 45%;
+    transform: translateY(-45%);
+  }
+  & p {
+    font-size: 2rem;
+    position: absolute;
+    top: 57%;
+    transform: translateY(-57%);
   }
   .product_bg {
     display: block;
@@ -102,14 +107,29 @@ const StProductHeaderContainer = styled.header`
     }
     .product_mbg {
       display: block;
-      width: 100%;
-      height: 100%;
+      height: 50rem;
       min-height: 40rem;
       min-width: 20rem;
     }
     & h2 {
       font-size: 3.5rem;
-      bottom: 7%;
+      top: 88%;
+      transform: translateY(-88%);
+    }
+    & p {
+      font-size: 1.5rem;
+      top: 93%;
+      transform: translateY(-93%);
+    }
+  }
+  @media screen and (max-width: 30rem) {
+    & h2 {
+      font-size: 3.5rem;
+      top: 92%;
+      transform: translateY(-92%);
+    }
+    & p {
+      display: none;
     }
   }
 `;
@@ -135,14 +155,19 @@ const StSelectCategoryContainer = styled.ul`
     flex-direction: column;
     align-items: center;
   }
-`;
+  `;
 // 카테고리 버튼
 const StCategoryButton = styled(Link)`
   width: 25rem;
+  min-height: 4rem;
   padding: 0.5rem 2rem;
   border: 0.1rem solid #f1f1f1;
   border-radius: 0.5rem;
-  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 1.3rem;
   transition: 0.3s;
   letter-spacing: 3px;
   &:hover {
