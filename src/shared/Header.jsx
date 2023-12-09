1b828da102939dba20363f8f8c1ec12704158c33
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-09 20:55:20 +0900

    [STYLE] header 반응형 css 추가

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,5 +1,8 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { FaStore } from 'react-icons/fa';
+import { GiArchiveResearch } from 'react-icons/gi';
+import { MdRestaurantMenu } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import paiksLogo from '../assets/img/header_logo.png';
 
@@ -11,15 +14,21 @@ const HeaderContainer = () => {
           <StPaiksCoffeeLogo src={paiksLogo} />
         </StLogoButton>
       </Link>
-      <Link to="/products">
-        <StHeaderButton>메뉴</StHeaderButton>
-      </Link>
-      <Link to="/location">
-        <StHeaderButton>매장 안내</StHeaderButton>
-      </Link>
-      <Link to="/interview">
-        <StHeaderButton>선호 조사</StHeaderButton>
-      </Link>
+      <StNavContainer>
+        <Link to="/products">
+          <StHeaderButton>메뉴</StHeaderButton>
+          <MdRestaurantMenu />
+        </Link>
+        <Link to="/location">
+          <StHeaderButton>매장 안내</StHeaderButton>
+          <FaStore />
+        </Link>
+
+        <Link to="/interview">
+          <StHeaderButton>선호 조사</StHeaderButton>
+          <GiArchiveResearch />
+        </Link>
+      </StNavContainer>
     </StHeaderStyle>
   );
 };
@@ -30,20 +39,40 @@ const StHeaderStyle = styled.div`
   font-size: 25px;
   font-weight: 600;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
-  padding-left: 300px;
-  padding-right: 300px;
   position: sticky;
+  gap: 5rem;
   top: 0;
   z-index: 10; //헤더 맨 앞으로 불러오는
   @media screen and (max-width: 75rem) {
     background-color: rgba(255, 255, 255, 0.8);
     display: flex;
-    flex-direction: column;
-    height: 320px;
-    padding-left: 20px;
-    padding-right: 20px;
+    flex-direction: row;
+    padding: 1rem;
+  }
+  @media screen and (max-width: 30rem) {
+    justify-content: space-between;
+    gap: 0;
+  }
+`;
+const StNavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+
+  color: var(--primaryColor);
+  a svg {
+    display: none;
+  }
+
+  @media screen and (max-width: 30rem) {
+    a svg {
+      display: block;
+    }
+    a button {
+      display: none;
+    }
   }
 `;
 
@@ -67,6 +96,9 @@ const StPaiksCoffeeLogo = styled.img`
   width: auto;
   @media screen and (max-width: 75rem) {
     width: 120px;
+  }
+  @media screen and (max-width: 30rem) {
+    margin-left: 0;
   }
 `;
 
