f90cac8f8068b05369e8efd3d9ce407f9bb9cae9
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-09 16:37:03 +0900

    [REFACTOR] redux로 전역 상태 관리

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import KaKaoMap from './components/location/KaKaoMap';
 import HomePage from './pages/HomePage';
 import InterviewPage from './pages/InterviewPage';
 import LocationPage from './pages/LocationPage';
@@ -17,7 +16,6 @@ function Router() {
           <Route path="/location" element={<LocationPage />} />
           <Route path="/interview" element={<InterviewPage />} />
         </Route>
-        <Route path="/test-location" element={<KaKaoMap />} />
       </Routes>
     </BrowserRouter>
   );
